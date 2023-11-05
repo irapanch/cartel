5d0b23b84cd42647eb672498b3eba3b12111c67c
irapanch <117355580+irapanch@users.noreply.github.com>
2023-11-05 19:09:33 +0200

    create HomePage template

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,29 @@ import Kharkiv from 'components/Kharkiv/Kharkiv';
 import Kyiv from 'components/Kyiv/Kyiv';
 import Pokrovsk from 'components/Pokrovsk/Pokrovsk';
 import React from 'react';
-import { StyledNavLink } from './HomePage.styled';
+import {
+  StyledAboutBtn,
+  StyledAboutBtnPhone,
+  StyledHomeSection,
+  StyledListCities,
+  StyledNavLink,
+} from './HomePage.styled';
+import { FaTelegram } from 'react-icons/fa';
+import { FaPhoneVolume } from 'react-icons/fa6';
 
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loader } from 'components/Loader/Loader';
 
+const phoneNumber = '+380994404466';
+
 const HomePage = () => {
+  const handleClickTelegram = () => {
+    window.open('https://t.me/cartel_obmen');
+  };
   return (
-    <>
-      <ul>
+    <StyledHomeSection>
+      <StyledListCities>
         <StyledNavLink to="kyiv">
           <Kyiv />
         </StyledNavLink>
@@ -21,11 +34,19 @@ const HomePage = () => {
         <StyledNavLink to="pokrovsk">
           <Pokrovsk />
         </StyledNavLink>
-      </ul>
+      </StyledListCities>
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
-    </>
+      <div className="phone">
+        <StyledAboutBtnPhone href={`tel:${phoneNumber}`}>
+          <FaPhoneVolume />
+        </StyledAboutBtnPhone>
+        <StyledAboutBtn onClick={handleClickTelegram} className="teleg">
+          <FaTelegram />
+        </StyledAboutBtn>
+      </div>
+    </StyledHomeSection>
   );
 };
 
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,22 +1,118 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-export const StyledNavLink = styled(NavLink)`
-  padding: 11px 16px;
-  border-radius: 4px;
-  text-decoration: none;
-  color: ${props => props.theme.colors.text};
-  font-weight: ${props => props.theme.fontWeights.medium};
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+import BgAboutMobile from '../../image/bgDesc.jpg';
 
-  :hover,
-  :focus {
-    color: ${props => props.theme.colors.accent};
-    box-shadow: 0 0 15px #2a2a2a;
+export const StyledHomeSection = styled.section`
+  position: relative;
+  width: 100dvw;
+  min-height: calc(100vh - 125px);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: ${props => props.theme.colors.cardBackground};
+  background-image: url(${BgAboutMobile});
+  display: flex;
+  flex-direction: column;
+  .phone {
+    position: fixed;
+    bottom: 25px;
+    right: 25px;
+    display: flex;
+    gap: 10px;
+  }
+`;
+
+export const StyledListCities = styled.ul`
+  margin-top: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  @media screen and (min-width: 768px) {
+    justify-content: flex-start;
+    margin-left: 10%;
+  }
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  padding: 5px 20px;
+  border-radius: 20px;
+  font-weight: ${props => props.theme.fontWeights.medium};
+  font-size: ${props => props.theme.fontSizes.ml};
+  background: ${props => props.theme.colors.gradient};
+  border: 2px solid ${props => props.theme.colors.gradient};
+  color: ${props => props.theme.colors.white};
+
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    border: 2px solid ${props => props.theme.colors.white};
+    box-shadow: ${props => props.theme.shadows.textShadow};
   }
 
   &.active {
     color: ${props => props.theme.colors.white};
-    /* background: ${props => props.theme.colors.gradient}; */
-    box-shadow: 0 0 15px #2a2a2a;
+    border: 2px solid ${props => props.theme.colors.white};
+    box-shadow: ${props => props.theme.shadows.textShadow};
+  }
+`;
+
+export const StyledAboutBtnPhone = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  border: 2px solid inherit;
+  background: ${props => props.theme.colors.gradient};
+  color: ${props => props.theme.colors.white};
+  cursor: pointer;
+  box-shadow: ${props => props.theme.shadows.textShadow};
+
+  &:hover,
+  &:focus {
+    border: 2px solid ${props => props.theme.colors.white};
+  }
+
+  > svg {
+    width: 27px;
+    height: 27px;
+  }
+`;
+export const StyledAboutBtn = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 5px 10px;
+  margin: auto;
+  border-radius: 20px;
+  border: 2px solid inherit;
+  font-weight: ${props => props.theme.fontWeights.medium};
+  font-size: ${props => props.theme.fontSizes.ml};
+  background: ${props => props.theme.colors.gradient};
+  color: ${props => props.theme.colors.white};
+  cursor: pointer;
+  box-shadow: ${props => props.theme.shadows.textShadow};
+
+  &:hover,
+  &:focus {
+    border: 2px solid ${props => props.theme.colors.white};
+  }
+  > svg {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: white;
+    border: 1px solid #00d9ff;
+    fill: #00d9ff;
+  }
+  &.teleg {
+    border-radius: 50%;
+    padding: 0;
+    > svg {
+      width: 45px;
+      height: 45px;
+    }
   }
 `;
